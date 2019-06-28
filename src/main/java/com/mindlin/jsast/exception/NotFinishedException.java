aa40e003e00cd077e150e44c8a40127bc96b8f3c
mailmindlin <liamttlyrox@gmail.com>
2019-06-28 13:39:16 -0400

    Fix group

--- src/main/java/com/mindlin/jsast/exception/NotFinishedException.java
+++ src/main/java/com/mindlin/jsast/exception/NotFinishedException.java
@@ -1,5 +1,31 @@
 package com.mindlin.jsast.exception;
 
+import com.mindlin.nautilus.fs.SourcePosition;
+
 public class NotFinishedException extends RuntimeException {
+
+	public NotFinishedException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NotFinishedException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NotFinishedException(String message, SourcePosition position) {
+		super(message);
+	}
+
+	public NotFinishedException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NotFinishedException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
 	
 }
